db6df908091d567c4c252728009f440863beb5f9
gdai_on_linux_n7 <gdai@roucool.enseeiht.fr>
2021-09-25 16:05:06 +0200

    re-upload by ubantu

---
+++ UE-Programmation Fonctionelle/TPs/TP1/sourceEtu/_build/default/pgcd.ml
@@ -0,0 +1,19 @@
+(*  Exercice à rendre **)
+(*  pgcd : int -> int -> int = <fun>
+Fonction qui calcule alcul du Plus Grand Commun Diviseur
+Paramètre "a" et "b": deux entiers positives à calculer
+Précondition : 
+Résultat : un entier positive de la résultat de PGCD 
+*)
+let rec pgcd a b = 
+if a = 0 then b else
+if b = 0 then b else
+if a >= b then pgcd (a mod b) b
+else pgcd a (b mod a)
+
+(*  TO DO : tests unitaires *)
+
+let%test _ = pgcd 0 1 = 1
+let%test _ = pgcd 1 0 = 1
+let%test _ = pgcd 1 1 = 1
+let%test _ = pgcd 2 4 = 2
